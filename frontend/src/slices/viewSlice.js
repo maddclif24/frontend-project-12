4867ae7346c3112a23c18b0666cec73736163c3a
Ayzek <Ayzekmanduar@mail.ru>
2022-08-30 23:01:58 +0900

    add viewSlice

---
+++ frontend/src/slices/viewSlice.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-param-reassign */
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  activeChannelId: null,
+};
+
+const viewSlice = createSlice({
+  name: 'view',
+  initialState,
+  reducers: {
+    setActiveChannelId: (state, actions) => {
+      console.log(actions);
+      state.activeChannelId = actions.payload;
+    },
+  },
+});
+export const { actions } = viewSlice;
+export default viewSlice.reducer;
