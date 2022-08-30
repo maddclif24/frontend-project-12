8d43d141c2665b7a4f8dccc0366ec257d4a65a01
Ayzek <Ayzekmanduar@mail.ru>
2022-08-30 22:56:04 +0900

    fix redux

--- frontend/src/slices/index.js
+++ frontend/src/slices/index.js
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import dataReducer from './dataSlices.js';
 import loginReducer from './loginSlice.js';
 import channelSlice from './channelSlice.js';
+import viewSlice from './viewSlice.js';
 
 export default configureStore({
   reducer: {
     data: dataReducer,
     userCurrent: loginReducer,
     channels: channelSlice,
+    viewSlice,
   },
 });
 console.log(configureStore({
