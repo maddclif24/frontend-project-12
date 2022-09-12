79e433d4012972c1826fc6bc3c19c81f2ff764dd
Ayzek <Ayzekmanduar@mail.ru>
2022-09-12 16:52:05 +0900

    add unique className

--- frontend/src/components/Chat/Modal/AddChannel.jsx
+++ frontend/src/components/Chat/Modal/AddChannel.jsx
@@ -5,13 +5,17 @@ import React, { useEffect, useRef } from 'react';
 import { useSelector, useDispatch, useStore } from 'react-redux';
 import { toast } from 'react-toastify';
 import { useFormik } from 'formik';
+import cn from 'classnames';
 import {
   Modal, Form, Button, CloseButton,
 } from 'react-bootstrap';
 import { io } from 'socket.io-client';
 import * as Yup from 'yup';
 import { useTranslation } from 'react-i18next';
-import { actions as channelActions } from '../../../slices/channelSlice.js';
+import {
+  selectors as channelSelectors,
+  actions as channelActions,
+} from '../../../slices/channelSlice.js';
 import { actions as viewActions } from '../../../slices/viewSlice.js';
 
 const socket = io();
@@ -19,11 +23,15 @@ const socket = io();
 const AddChannel = ({ show, close }) => {
   const dispacth = useDispatch();
 
+  const channels = useSelector(channelSelectors.selectAll);
+  const channelsNames = channels.map((channel) => channel.name);
+
   const channelSchema = Yup.object().shape({
     name: Yup.string()
-      .max(20, 'От 3 до 20 символов'),
+      .min(3, 'От 3 до 20 символов')
+      .max(20)
+      .notOneOf(channelsNames, 'Должен быть уникальным'),
   });
-
   const { t } = useTranslation();
 
   const formik = useFormik({
@@ -32,17 +40,17 @@ const AddChannel = ({ show, close }) => {
     },
     validationSchema: channelSchema,
     onSubmit: (values) => {
-      console.log(values);
       const channel = { name: values.name.trim() };
       socket.emit('newChannel', channel);
       socket.on('newChannel', (payload) => {
-        console.log(payload);
         dispacth(viewActions.switchActiveChannel(payload.id));
         dispacth(channelActions.addNewChannel(payload));
       });
+      close();
       toast.success(t('tostify.successAdd'));
     },
   });
+  console.log(formik.errors);
 
   return (
     <>
@@ -60,7 +68,12 @@ const AddChannel = ({ show, close }) => {
                 required
                 onChange={formik.handleChange}
                 value={formik.values.name}
+                className={cn(
+                  'form-control',
+                  formik.errors.name ? 'is-invalid' : 'valid',
+                )}
               />
+              { formik.errors?.name ? <div className="invalid-feedback">{formik.errors.name}</div> : null}
             </Form.Group>
           </Form>
         </Modal.Body>
@@ -68,7 +81,7 @@ const AddChannel = ({ show, close }) => {
           <Button onClick={close} variant="secondary">
             {t('chatPage.channels.modalAdd.close')}
           </Button>
-          <Button type="submit" form="ChannelSubmit" onClick={close} variant="primary">
+          <Button type="submit" form="ChannelSubmit" variant="primary">
             {t('chatPage.channels.modalAdd.submit')}
           </Button>
         </Modal.Footer>
